96106cdae7c80448120aceafd72774d7d275ce48
shubham kumar <suraj2002fake@gmail.com>
2024-08-07 21:35:51 +0530

    Update HtmlSanitizer.java

--- src/main/java/com/on5Aug/services/HtmlSanitizer.java
+++ src/main/java/com/on5Aug/services/HtmlSanitizer.java
@@ -14,7 +14,7 @@ public class HtmlSanitizer {
                 "colgroup", "dd", "div", "dl", "dt", "em", "h1", "h2", "h3", "h4", "h5", "h6",
                 "i", "img", "li", "ol", "p", "pre", "q", "small", "span", "strike", "strong",
                 "sub", "sup", "table", "tbody", "td", "tfoot", "th", "thead", "tr", "u",
-                "ul"
+                "ul", "hr"
         );
 
         Safelist safeList = Safelist.basicWithImages();
